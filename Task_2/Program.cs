274cd0997db30908c2016288f84a989dad93b800
MaximBabenkov <lostbass@mail.ru>
2022-05-29 23:28:37 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу масштабирования фигуры
+Console.WriteLine("Enter a height of rectangle");
+int h = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Enter a width of rectangle");
+int w = int.Parse(Console.ReadLine()??"0");
+for (int i=0;i<h;i++)
+{
+    for (int j=0;j<w;j++)
+    {
+        Console.Write("@");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Enter a zoom factor");
+double k = double.Parse(Console.ReadLine()??"0");
+for (int i=0;i<(h*k);i++)
+{
+    for (int j=0;j<(w*k);j++)
+    {
+        Console.Write("@");
+    }
+    Console.WriteLine();
+}
